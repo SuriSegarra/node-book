7f0b74f4be168c454d571bdee203f39ee7a2ed55
surelis segarra <sdarianne34@gmail.com>
2020-03-22 22:36:36 -0700

    changing the route

--- router.js
+++ router.js
@@ -1,5 +1,10 @@
 //our router
 function route(pathname) {
     console.log('About to route a request for ' + pathname);
+    if(typeof handle[pathname] === 'funtion') {
+        handle[pathname]();
+    } else {
+        console.log('no request handler found for' + pathname);
+    }
 }
 exports.route = route;
