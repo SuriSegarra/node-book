a6e9c30a446fe41d70ba43f6b94b3621bc3d17c0
surelis segarra <sdarianne34@gmail.com>
2020-03-23 08:25:24 -0700

    handling POST req

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,18 +1,28 @@
 
 var exec = require('child_process').exec;
 
-function start(res) {
-     console.log("Request handler 'start' was called.");
-  
-     exec('find/',
-     { timeout: 10000, maxBuffer: 20000*1024 },
-     function(error, stdout, stderr) {
+
+function start(response) {
+    console.log("Request handler 'start' was called.");
+    
+    const body = '<html>'
+    '<head>'
+    '<meta http-equiv="Content-Type" '
+    'content="text/html; charset=UTF-8" />'
+    '</head>'
+    '<body>'
+    '<form action="/upload" enctype="multipart/form-data" '
+    'method="post">'
+    '<input type="file" name="upload" multiple="multiple">'
+    '<input type="submit" value="Upload file" />'
+    '</form>'
+    '</body>'
+    '</html>';
+    
          res.writeHead(200, {"Content-Type": "text/plain"});
          res.write(stdout);
          res.end();
-     })
-
-     };
+     }
      function upload(res) {
     console.log("Request handler 'upload' was called.");
     res.writeHead(200, {"Content-Type": "text/plain"});
