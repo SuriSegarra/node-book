c41ea059c7c9266db2fef6d252d05551b3877b77
surelis segarra <sdarianne34@gmail.com>
2020-03-23 08:19:29 -0700

    added to req handlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,55 +1,28 @@
-const querystring = require("querystring"),
-fs = require("fs"),
-formidable = require("formidable");
 
-function start(response) {
-console.log("Request handler 'start' was called.");
+var exec = require('child_process').exec;
 
-const body = '<html>'
-'<head>'
-'<meta http-equiv="Content-Type" '
-'content="text/html; charset=UTF-8" />'
-'</head>'
-'<body>'
-'<form action="/upload" enctype="multipart/form-data" '
-'method="post">'
-'<input type="file" name="upload" multiple="multiple">'
-'<input type="submit" value="Upload file" />'
-'</form>'
-'</body>'
-'</html>';
+function start(res) {
+     console.log("Request handler 'start' was called.");
+    var content = empty; 
 
-response.writeHead(200, {"Content-Type": "text/html"});
-response.write(body);
-response.end();
+    exec('ls -lah', function(error, stdout, stderr) {
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write(stdout);
+        res.end();
+    })
 }
+// function sleep(milliSeconds) {
+//  var startTime = new Date().getTime();
+//  while (new Date().getTime() < startTime + milliSeconds);
+//  }
 
-function upload(response, request) {
-console.log("Request handler 'upload' was called.");
-
-const form = new formidable.IncomingForm();
-console.log("about to parse");
-form.parse(request, function(error, fields, files) {
-console.log("parsing done");
-fs.rename(files.upload.path, "/tmp/test.png", function(error) {
-if (error) {
-fs.unlink("/tmp/test.png");
-fs.rename(files.upload.path, "/tmp/test.png");
+    
+     function upload(res) {
+    console.log("Request handler 'upload' was called.");
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write('hello upload');
+    res.end();
 }
-});
-response.writeHead(200, {"Content-Type": "text/html"});
-response.write("received image:<br/>");
-response.write("<img src='/show' />");
-response.end();
-});
-}
-
-function show(response) {
-console.log("Request handler 'show' was called.");
-response.writeHead(200, {"Content-Type": "image/png"});
-fs.createReadStream("/tmp/test.png").pipe(response);
-}
-
+    
 exports.start = start;
 exports.upload = upload;
-exports.show = show;
