2824795a60fd4ddfba2d49acb7c08bd2cb8c496e
surelis segarra <sdarianne34@gmail.com>
2020-03-23 08:22:05 -0700

    modifying reqhandlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,20 +3,16 @@ var exec = require('child_process').exec;
 
 function start(res) {
      console.log("Request handler 'start' was called.");
-    var content = empty; 
+  
+     exec('find/',
+     { timeout: 10000, maxBuffer: 20000*1024 },
+     function(error, stdout, stderr) {
+         res.writeHead(200, {"Content-Type": "text/plain"});
+         res.write(stdout);
+         res.end();
+     })
 
-    exec('ls -lah', function(error, stdout, stderr) {
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.write(stdout);
-        res.end();
-    })
-}
-// function sleep(milliSeconds) {
-//  var startTime = new Date().getTime();
-//  while (new Date().getTime() < startTime + milliSeconds);
-//  }
-
-    
+     };
      function upload(res) {
     console.log("Request handler 'upload' was called.");
     res.writeHead(200, {"Content-Type": "text/plain"});
